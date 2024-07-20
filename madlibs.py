a23ce90a1984563f2925d70e7c11cad5196de09b
Shashwat Sharma <shashwatsharma850@gmail.com>
2024-07-20 08:23:04 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+# youtuber="ana"
+# print("subscribe to",youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj=input("Adj:")
+verb=input("Verb:")
+verb2=input("Verb:")
+famper=input("Famper:")
+
+madlib=f"Computer programming is so {adj}! It makes me so excited all the time because I love to {Verb}. Stay hydrated and {verb2} like you are {famper}"
+
+print(madlib)
